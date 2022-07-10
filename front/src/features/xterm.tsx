fff4991aaf65ffdb017b4953e4e8aa2bdf0b3ed6
haruka <onemanlive0603tame@yahoo.ne.jp>
2022-07-10 13:37:34 +0000

    refs commit

--- front/src/features/xterm.tsx
+++ front/src/features/xterm.tsx
@@ -1,14 +1,17 @@
 import { useEffect, useRef } from 'react'
 import { Terminal } from 'xterm'
+// import { FitAddon } from 'xterm-addon-fit'
+// import { WebLinksAddon } from 'xterm-addon-web-links'
 
 export default function Xterm() {
   const xtermRef = useRef<Terminal>(null!)
+  //   const [fitAddon] = useState(new FitAddon())
 
   useEffect(() => {
     if (xtermRef == null) {
       return
     }
-    console.log('hogehoge')
+    console.log('inicialize xterm')
     const initTerminal = async () => {
       const { Terminal } = await import('xterm')
       const { FitAddon } = await import('xterm-addon-fit')
@@ -23,22 +26,21 @@ export default function Xterm() {
       fitAddon.fit()
 
       const shellprompt = '$ '
-      xtermRef.current.prompt = () => {
-        xtermRef.current.write('\r\n' + shellprompt)
-      }
-      //your greeting
-      xtermRef.current.prompt()
+      xtermRef.current.write('\r\n' + shellprompt)
+
       let cmd = ''
 
       xtermRef.current.onKey((key) => {
         const char = key.domEvent.key
         if (char === 'Enter' && cmd.length > 0) {
           switch (
-            cmd
+            (console.log(cmd), cmd)
             //your cmd logic
           ) {
           }
-          xtermRef.current.prompt()
+          //   xtermRef.current.write(cmd)
+          xtermRef.current.write('\r\n')
+          xtermRef.current.write('$ ')
           cmd = ''
         } else if (char === 'Backspace') {
           xtermRef.current.write('\b \b')
@@ -49,12 +51,13 @@ export default function Xterm() {
         }
       })
       // Add logic
-      return xtermRef.current
+      return xtermRef
     }
     const xterm = initTerminal()
-    return async () => {
-      console.log(xterm)
-      ;(await xterm).reset()
+    return () => {
+      // xterm.then(console.log)
+      // xtermRef.current.dispose()
+      xterm.then((x) => x.current.dispose())
     }
   }, [])
   return (
