fb8b29b68dc693a162a558831660265be2753763
haruka <onemanlive0603tame@yahoo.ne.jp>
2022-07-30 08:41:14 +0000

    コンポーネント作成

--- front/src/parts/card.tsx
+++ front/src/parts/card.tsx
@@ -1,5 +1,43 @@
-const Card = () => {
-  return <></>
-}
+import {
+  Box,
+  Center,
+  useColorModeValue,
+  Heading,
+  Text,
+  Stack,
+} from '@chakra-ui/react'
 
-export default Card
+export default function Card() {
+  return (
+    <Center py={12}>
+      <Box
+        role={'group'}
+        p={6}
+        maxW={'330px'}
+        w={'full'}
+        bg={useColorModeValue('white', 'gray.800')}
+        boxShadow={'2xl'}
+        rounded={'lg'}
+        pos={'relative'}
+        zIndex={1}
+      >
+        <Stack pt={10} align={'center'}>
+          <Text color={'gray.500'} fontSize={'sm'} textTransform={'uppercase'}>
+            Branch
+          </Text>
+          <Heading fontSize={'2xl'} fontFamily={'body'} fontWeight={500}>
+            Main
+          </Heading>
+          <Stack direction={'row'} align={'center'}>
+            <Text fontWeight={800} fontSize={'xl'}>
+              10日前
+            </Text>
+            <Text textDecoration={'line-through'} color={'gray.600'}>
+              {/* $199 */}
+            </Text>
+          </Stack>
+        </Stack>
+      </Box>
+    </Center>
+  )
+}
--- front/src/parts/consoleDisplay.tsx
+++ front/src/parts/consoleDisplay.tsx
@@ -1,5 +1,59 @@
-const ConsoleDisplay = () => {
-  return <></>
-}
+import { Box } from '@chakra-ui/layout'
+import React, { useEffect, useRef } from 'react'
+import { Terminal } from 'xterm'
+import 'xterm/css/xterm.css'
 
-export default ConsoleDisplay
+export const isBrowser = typeof window !== 'undefined'
+// export const ws = isBrowser
+//   ? new WebSocket('ws://localhost:8080/socket.io')
+//   : null
+export default function ConsoleDisplay() {
+  const xtermRef = useRef<Terminal>(null!)
+
+  useEffect(() => {
+    if (xtermRef == null) {
+      return
+    }
+    const initTerminal = async () => {
+      const { Terminal } = await import('xterm')
+      const { FitAddon } = await import('xterm-addon-fit')
+      const { WebLinksAddon } = await import('xterm-addon-web-links')
+      const fitAddon = new FitAddon()
+      xtermRef.current = new Terminal({
+        cursorBlink: true,
+      })
+      xtermRef.current.loadAddon(fitAddon)
+      xtermRef.current.loadAddon(new WebLinksAddon())
+      xtermRef.current.open(document.getElementById('terminal') as HTMLElement)
+      fitAddon.fit()
+
+      let cmd = ''
+
+      xtermRef.current.onKey((key) => {
+        const char = key.domEvent.key
+        if (char === 'Enter' && cmd.length > 0) {
+          xtermRef.current.write('\r\n')
+          xtermRef.current.write('$ ')
+          cmd = ''
+        } else if (char === 'Backspace') {
+          xtermRef.current.write('\b \b')
+          cmd = cmd.slice(0, cmd.length - 1)
+        } else {
+          xtermRef.current.write(char)
+          cmd += char
+        }
+      })
+      return xtermRef
+    }
+    const xterm = initTerminal()
+    return () => {
+      xterm.then((x) => x.current.dispose())
+    }
+  }, [])
+
+  return (
+    <>
+      <Box id="terminal" className="" />
+    </>
+  )
+}
