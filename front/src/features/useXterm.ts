e862f72fa95b9d2201cef9222475999f0d6252f3
haruka <onemanlive0603tame@yahoo.ne.jp>
2022-07-31 13:41:12 +0000

    カスタムフック化

---
+++ front/src/features/useXterm.ts
@@ -0,0 +1,67 @@
+import { useEffect, useRef } from 'react'
+import { Terminal } from 'xterm'
+
+export const useXterm = (
+  targetIdName: string,
+  enteredLogic?: (cmd: string) => void,
+) => {
+  const xtermRef = useRef<Terminal>(null!)
+
+  useEffect(() => {
+    if (xtermRef == null) {
+      return
+    }
+    if (typeof enteredLogic === 'undefined') {
+      return
+    }
+    const initTerminal = async () => {
+      const { Terminal } = await import('xterm')
+      const { FitAddon } = await import('xterm-addon-fit')
+      const { WebLinksAddon } = await import('xterm-addon-web-links')
+      const fitAddon = new FitAddon()
+      xtermRef.current = new Terminal({
+        cursorBlink: true,
+      })
+      xtermRef.current.loadAddon(fitAddon)
+      xtermRef.current.loadAddon(new WebLinksAddon())
+      xtermRef.current.open(
+        document.getElementById(targetIdName) as HTMLElement,
+      )
+      fitAddon.fit()
+
+      let cmd = ''
+
+      xtermRef.current.onKey((key) => {
+        const char = key.domEvent.key
+        if (char === 'Enter' && cmd.length > 0) {
+          enteredLogic(cmd)
+          xtermRef.current.write('\r\n')
+          xtermRef.current.write('$ ')
+          cmd = ''
+        } else if (char === 'Backspace') {
+          xtermRef.current.write('\b \b')
+          cmd = cmd.slice(0, cmd.length - 1)
+        } else {
+          xtermRef.current.write(char)
+          cmd += char
+        }
+      })
+      return xtermRef
+    }
+    const xterm = initTerminal()
+    return () => {
+      xterm.then((x) => x.current.dispose())
+    }
+  }, [enteredLogic, targetIdName])
+
+  return [xtermRef.current?.write]
+}
+
+export const Hoge = () => {
+  const send = (cmd: string) => {
+    console.log(cmd)
+  }
+
+  const [terminalWrite] = useXterm('terminal', send)
+  console.log(terminalWrite)
+}
--- front/src/parts/consoleDisplay.tsx
+++ front/src/parts/consoleDisplay.tsx
@@ -1,55 +1,15 @@
+import { useXterm } from '@/features/useXterm'
 import { Box } from '@chakra-ui/layout'
-import React, { useEffect, useRef } from 'react'
-import { Terminal } from 'xterm'
+import React from 'react'
 import 'xterm/css/xterm.css'
 
 export const isBrowser = typeof window !== 'undefined'
-// export const ws = isBrowser
-//   ? new WebSocket('ws://localhost:8080/socket.io')
-//   : null
+
 export default function ConsoleDisplay() {
-  const xtermRef = useRef<Terminal>(null!)
-
-  useEffect(() => {
-    if (xtermRef == null) {
-      return
-    }
-    const initTerminal = async () => {
-      const { Terminal } = await import('xterm')
-      const { FitAddon } = await import('xterm-addon-fit')
-      const { WebLinksAddon } = await import('xterm-addon-web-links')
-      const fitAddon = new FitAddon()
-      xtermRef.current = new Terminal({
-        cursorBlink: true,
-      })
-      xtermRef.current.loadAddon(fitAddon)
-      xtermRef.current.loadAddon(new WebLinksAddon())
-      xtermRef.current.open(document.getElementById('terminal') as HTMLElement)
-      fitAddon.fit()
-
-      let cmd = ''
-
-      xtermRef.current.onKey((key) => {
-        const char = key.domEvent.key
-        if (char === 'Enter' && cmd.length > 0) {
-          xtermRef.current.write('\r\n')
-          xtermRef.current.write('$ ')
-          cmd = ''
-        } else if (char === 'Backspace') {
-          xtermRef.current.write('\b \b')
-          cmd = cmd.slice(0, cmd.length - 1)
-        } else {
-          xtermRef.current.write(char)
-          cmd += char
-        }
-      })
-      return xtermRef
-    }
-    const xterm = initTerminal()
-    return () => {
-      xterm.then((x) => x.current.dispose())
-    }
-  }, [])
+  const send = (cmd: string) => {
+    console.log(cmd)
+  }
+  const [] = useXterm('terminal', send)
 
   return (
     <>
